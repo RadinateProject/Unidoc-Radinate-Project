2996fd1a675cd1adc07fdfbc1c6ffed871d87bbc
Karan02 <krnptl1234@gmail.com>
2025-12-15 18:09:29 +0530

    fetch api mismatch

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -11,7 +11,7 @@ export async function apiFetch(endpoint: string, options: RequestInit = {}) {
 
   const response = await fetch(
     // ${process.env.NEXT_PUBLIC_API_URL}
-  'api/'+
+  '/api'+
     `${endpoint}`, {
     ...options,
     headers,
